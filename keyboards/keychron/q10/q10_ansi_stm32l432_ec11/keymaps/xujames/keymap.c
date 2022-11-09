c56f56965bb3e4bd12cf4d7339dfdf0c881a4efd
James Xu <xujames@users.noreply.github.com>
2022-11-08 23:52:09 -0500

    Q10 Keymap: add numpad

--- keyboards/keychron/q10/q10_ansi_stm32l432_ec11/keymaps/xujames/keymap.c
+++ keyboards/keychron/q10/q10_ansi_stm32l432_ec11/keymaps/xujames/keymap.c
@@ -61,15 +61,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         UCX_MAINS_PTM,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
         UCX_AN1_PWR,    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
         UCX_MAINS_VOLU, KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
-        UCX_MAINS_VOLD, KC_LCTL,  KC_LOPT,            KC_LCMD,  KC_SPC,  MO(MAC_FN),                     KC_SPC,             KC_RCMD,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
+        UCX_MAINS_VOLD, KC_LCTL,  KC_LOPT,            KC_LCMD,  KC_SPC,  MO(MAC_FN),                     KC_SPC,             MO(MAC_FN),                   KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_89(
-        _______,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  _______,  RGB_RMOD, RGB_VAD,  KC_DFU,   RGB_SAD,  RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-        _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  _______,  _______,            _______,  _______,  _______,                       _______,            _______,                      _______,  _______,  _______),
+        _______,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   KC_7,     KC_8,     KC_9,     KC_PPLS,  _______,  _______,   _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  KC_DFU,   _______,  _______,   KC_4,     KC_5,     KC_6,     KC_PPLS,  _______,  _______,             _______,            _______,
+        _______,  _______,            _______,  _______,  _______,  _______,   _______,  KC_PPLS,  KC_1,     KC_2,     KC_3,     KC_PENT,   _______,  _______,  _______,
+        _______,  _______,  _______,            _______,  _______,  _______,                       KC_0,               KC_PDOT,                       _______,  _______,  RGB_TOG),
 
     [DVORAK_BASE] = LAYOUT_ansi_89(         // WIN key switch
         UCX_AN1_TOG,    KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_INS,             KC_DEL,
@@ -77,15 +77,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         UCX_MAINS_PTM,  KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,      KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,  KC_EQL,   KC_BSLS,            KC_PGDN,
         UCX_AN1_PWR,    KC_CAPS,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,      KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,            KC_ENT,             KC_HOME,
         UCX_MAINS_VOLU, KC_LSFT,            KC_SCLN,  KC_Q,     KC_J,     KC_K,      KC_X,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,     KC_RSFT,  KC_UP,
-        UCX_MAINS_VOLD, KC_LCTL,  KC_LCMD,            KC_LALT,  KC_SPC,  MO(DVORAK_FN),                     KC_SPC,             KC_RALT,                   KC_LEFT,  KC_DOWN,  KC_RGHT),
+        UCX_MAINS_VOLD, KC_LCTL,  KC_LOPT,            KC_LCMD,  KC_SPC,  MO(DVORAK_FN),                  KC_SPC,             MO(DVORAK_FN),                KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [DVORAK_FN] = LAYOUT_ansi_89(           // WIN key switch
         _______,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,
-        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,            _______,
-        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,             _______,            _______,
-        _______,  _______,            _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,
-        _______,  _______,  _______,            _______,  _______,  _______,                       _______,            _______,                       _______,  _______,  _______),
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,   KC_7,     KC_8,     KC_9,     KC_PPLS,  _______,  _______,   _______,  _______,            _______,
+        _______,  _______,  _______,  _______,  KC_DFU,   _______,  _______,   KC_4,     KC_5,     KC_6,     KC_PPLS,  _______,  _______,             _______,            _______,
+        _______,  _______,            _______,  _______,  _______,  _______,   _______,  KC_PPLS,  KC_1,     KC_2,     KC_3,     KC_PENT,   _______,  _______,  _______,
+        _______,  _______,  _______,            _______,  _______,  _______,                       KC_0,               KC_PDOT,                       _______,  _______,  RGB_TOG),
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
